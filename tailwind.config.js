de7c485778e6f83e027eba269fe574146fa11cd7
Mohammed Lahboub <104053591+mohammedlahboub@users.noreply.github.com>
2023-01-23 15:15:05 +0100

    first commit : bio-link almost done

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'textW': '#F1FAEE',
+        'mainBg': '#BFC0FF',
+        'profileBg': '#14213D',
+        'textcol-1': '#8338EC',
+        'textcol-2': '#FB8500',
+        'linkBg': '#001219',
+        'linkBg-2': '#FCA311',
+        'linkBorder': '#363A3C',
+        'linkBorder-2': '#FB8500',
+
+      },
+      fontFamily: {
+        'fredokaOne': ['Fredoka One', 'sans-serif'],
+      },
+      backgroundImage: {
+        'main-bg': "",
+
+      }
+    },
+  },
+  plugins: [],
+}
